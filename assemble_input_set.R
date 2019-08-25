70942ec9b540172a8cdc7a00a15a3d9563600bca
Steven R. Bates <steverbates@gmail.com>
2019-08-24 17:55:42 -0700

    Add files via upload

---
+++ assemble_input_set.R
@@ -0,0 +1,55 @@
+#R implementation of assemble_input_set.py
+
+frame_split <- function(frame,fold) { #trying to approximate behavior of np.array_split in R with data frame input
+	n <- nrow(frame)
+	r <- n %% fold
+	q <- n %/% fold
+	x <- list()
+	if (r != 0) {
+		for (i in 1:r) {
+		x[[i]] <- frame[((q+1)*(i-1)+1):((q+1)*i),] #nrow = quotient plus an extra item for a number of subframes equal to remainder
+		}
+	}
+	for (i in (r+1):fold) {
+		x[[i]] <- frame [(q*(i-1)+1):(q*i),]  # nrow = quotient for the rest of the subframes requested
+	}
+	return(x)
+}
+
+assemble_input_set <- function(positives,negatives,fold=1) { #to balance populations of two outcomes for twofold classificiation problems. Assumes dataframe inputs.  Fold parameter used to generate a set of samples for cross-validation
+	n <- nrow(negatives)
+	p <- nrow(positives)
+	positives$Survival <- rep(1,nrow(positives))
+	negatives$Survival <- rep(0,nrow(negatives))
+	if (fold==1) {
+		if (n<p) {
+			x <- rbind(positives[sample(1:nrow(positives),size=nrow(negatives)),],negatives)
+		}
+		else {
+			x <- rbind(positives,negatives[sample(1:nrow(negatives),size=nrow(positives)),])
+		}
+		return(x[sample(1:nrow(x)),]) #shuffling positive and negative data before output; otherwise fit function would take validation split from back end i.e. all negative data
+	} else {
+		if (n<p) {
+			P <- frame_split(positives[sample(1:nrow(negatives)),],fold)
+			N <- frame_split(negatives,fold)
+		}
+		else {
+			P <- frame_split(positives,fold)
+			N <- frame_split(negatives[sample(1:nrow(positives)),],fold)			
+		}
+		x <- list()#initializating list of dataframes here
+		for (i in 1:length(P)) {
+			X <- rbind(P[[i]],N[[i]])
+			x[[i]] <- X[sample(1:nrow(X)),]
+		}
+		return(x)
+	}
+}
+
+
+
+
+
+
+
---
+++ assemble_input_set.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import random
+
+'''
+A helper function to take an unbalanced data set for a two-fold classification problem (two possible classes: positive or negative) and effectively balance it, producing a data set with the same population size for each class.  The class with the smaller population in the unbalanced data set will be unaltered in the balanced data set, while a sample of the same size will be randomly chosen without replacement from the other class.
+'''
+
+
+def assemble_input_set(positives,negatives,fold=1): #Assumes dataframe inputs.  Fold parameter used to generate a set of samples for cross-validation
+	n,p = len(negatives),len(positives)
+	if fold==1:
+		if n<p:
+			x = np.concatenate((positives.sample(n=n,random_state=2127365000).values,negatives.values))
+			y = np.concatenate((np.ones(n),np.zeros(n)))
+			order = np.arange(2*n) 
+		else:
+			x = np.concatenate((positives.values,negatives.sample(n=p,random_state=2127365000).values))
+			y = np.concatenate((np.ones(p),np.zeros(p)))
+			order = np.arange(2*p)
+		np.random.shuffle(order)
+		return x[order,:],y[order] #shuffling positive and negative data before output; otherwise fit function would take validation split from back end i.e. all negative data
+	else:
+		if n<p:
+			P = np.array_split(positives.sample(n=n,random_state=2127365000).values,fold)
+			N = np.array_split(negatives.values,fold)
+		else:
+			P = np.array_split(positives.values,fold)
+			N = np.array_split(negatives.sample(n=p,random_state=2127365000).values,fold)
+		x,y = [],[]
+		for i in range(len(P)):
+			X = np.concatenate((P[i],N[i]))
+			q = int(len(X)/2)
+			Y = np.concatenate((np.ones(q),np.zeros(q)))
+			order = np.arange(2*q)
+			np.random.shuffle(order)
+			x.append(X[order,:])
+			y.append(Y[order])
+		return x,y
